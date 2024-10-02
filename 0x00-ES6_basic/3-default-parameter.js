1c6e59254b7365ce87acc5a05fd2b072926628df
yassineedev <yassine.boussif18@gmail.com>
2024-10-02 16:02:59 +0100

    edit task 3

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -4,6 +4,6 @@
 
 // export default getSumOfHoods;
 
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 =19) {
- return initialNumber + expansion1989 + expansion2019;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
 }
